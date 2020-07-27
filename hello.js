14f3da1bb2086dfcf8d678fb99bda1528f874bb6
Iyris Vigil <iyrisvigil@gmail.com>
2020-07-27 22:52:12 +0000

    return and console log examples

--- hello.js
+++ hello.js
@@ -3,9 +3,23 @@
 }
 
 sayHello();*/
-
+/* 
 const sayHello = function (name) {
   console.log('Hello, ' + name);
 }
 
 sayHello();
+ */
+
+const sayHelloToConsole = function (name) {
+  console.log('Hello, ' + name);
+}
+
+sayHelloToConsole ('John');
+
+const returnSayHello = function (name) {
+  return 'Hello, ' + name;
+} 
+
+const greeting = returnSayHello('John');
+console.log(greeting);
